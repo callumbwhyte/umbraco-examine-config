a959f0cdec356c14725dd79392822bdf4c6beea9
Callum Whyte <hey@callumwhyte.com>
2020-06-25 13:40:29 +0100

    Defining interface and abstract class for Umbraco index configuration

---
+++ src/Our.Umbraco.ExamineConfig/IUmbracoIndexConfig.cs
@@ -0,0 +1,12 @@
+﻿using Examine.Config;
+
+namespace Our.Umbraco.ExamineConfig
+{
+    public interface IUmbracoIndexConfig : IIndexConfig
+    {
+        bool IncludePublished { get; }
+        bool IncludeProtected { get; }
+        string[] IncludeTypes { get; }
+        string[] ExcludeTypes { get; }
+    }
+}
---
+++ src/Our.Umbraco.ExamineConfig/UmbracoIndexConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Examine.Config;
+
+namespace Our.Umbraco.ExamineConfig
+{
+    public abstract class UmbracoIndexConfig : IUmbracoIndexConfig
+    {
+        public abstract string Name { get; }
+
+        public virtual IEnumerable<IIndexField> IncludeFields { get; }
+
+        public virtual IEnumerable<IIndexField> ExcludeFields { get; }
+
+        public virtual Type Type { get; }
+
+        public virtual string Path { get; }
+
+        public virtual Type Analyzer { get; }
+
+        public virtual bool IncludePublished { get; } = true;
+
+        public virtual bool IncludeProtected { get; } = false;
+
+        public virtual string[] IncludeTypes { get; }
+
+        public virtual string[] ExcludeTypes { get; }
+    }
+}
