de789df074b2550b4213e38a28f42577537decbe
Callum Whyte <hey@callumwhyte.com>
2020-06-25 13:56:28 +0100

    Registering indexes and searchers from examine.config in collections

---
+++ src/Our.Umbraco.ExamineConfig/Startup/ConfigComposer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using Examine.Config;
+using Our.Umbraco.ExamineConfig.Composing;
+using Umbraco.Core.Composing;
+using Umbraco.Web.Search;
+
+namespace Our.Umbraco.ExamineConfig.Startup
+{
+    [ComposeAfter(typeof(ExamineComposer))]
+    public class ConfigComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            RegisterConfig(composition);
+        }
+
+        private void RegisterConfig(Composition composition)
+        {
+            var examineSection = ConfigurationManager.GetSection("examine");
+
+            if (!(examineSection is IExamineConfig examineConfig))
+            {
+                throw new Exception("Failed to load Examine config");
+            }
+
+            foreach (var index in examineConfig.Indexes)
+            {
+                composition.Indexes().Add(index);
+            }
+
+            foreach (var searcher in examineConfig.Searchers)
+            {
+                composition.Searchers().Add(searcher);
+            }
+        }
+    }
+}
